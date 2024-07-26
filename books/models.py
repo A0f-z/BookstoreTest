9e0cea8e1fb94505a04e850a50d58f2017c648d7
Abolfazl Ahmadzadeh <a0fz.eyh@gmail.com>
2024-07-26 20:08:59 +0330

    model created for book

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __unicode__(self):
+        return self.title
