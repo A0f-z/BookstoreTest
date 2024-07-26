5064bf24f305a1624d18828ace98aa5f0b362b56
Abolfazl Ahmadzadeh <a0fz.eyh@gmail.com>
2024-07-26 20:52:42 +0330

    created views,urls and update model 'book to Book '

--- books/models.py
+++ books/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class book(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     description = models.TextField()
@@ -9,3 +9,4 @@ class book(models.Model):
 
     def __str__(self):
         return f'{self.title} |  {self.author}'
+
